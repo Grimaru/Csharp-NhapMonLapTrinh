3de6e18b9772a47b091abc40f28f1f538a5930bd
Grim <92217315+Grimaru@users.noreply.github.com>
2024-08-01 10:18:03 +0700

    Add files via upload

---
+++ Testing10.10/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing10._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();           
+            int m = int.Parse(s.Split(' ')[0]);
+            int n = int.Parse(s.Split(' ')[1]);
+            int[,] a = new int[m, n];
+            NhapMang(a, m, n);           
+            XuatMang(a, m, n);                                 
+            Console.ReadKey();
+        }
+
+        static void NhapMang(int[,] a, int m, int n)
+        {
+            for (int i = 0; i < m; i++)
+            {
+                string s = Console.ReadLine();
+                for (int j = 0; j < n; j++)
+                {
+                    a[i, j] = int.Parse(s.Split(' ')[i]);
+                }
+            }
+        }
+        static void XuatMang(int[,] a, int m, int n)
+        {
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write($"{a[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+    }
+}
---
+++ Testing10.13/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing10._13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare the array of two elements.
+            int[][] arr = new int[4][];
+
+            // Initialize the elements.
+            arr[0] = new int[3] { 5, 4, 7 };
+            arr[1] = new int[5] { 1, 7, 9, 0, 2 };
+            arr[2] = new int[4] { 7, 5, 8, 0 };
+            arr[3] = new int[2] { 7, 9 };
+
+            // Display the array elements.
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(" ", i);
+
+                for (int j = 0; j < arr[i].Length; j++)
+                {
+                    Console.Write("{0}{1}", arr[i][j], j == (arr[i].Length - 1) ? "" : " ");
+                }
+                Console.WriteLine();
+            }
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
+
